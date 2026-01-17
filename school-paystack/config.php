f70f3d22209558616cd5deab77f26211de9f096f
Big Quams <bigquamsmedia024@gmail.com>
2026-01-17 11:01:53 +0100

    Create config.php

---
+++ school-paystack/config.php
@@ -0,0 +1,4 @@
+<?php
+define("PAYSTACK_SECRET", "sk_live_7bdb0f70acc96ba9c4e15936c7ea4a8eac14ef93");
+define("ADMIN_PHONE", "2349049871643"); // WhatsApp number
+define("SCHOOL_EMAIL", "payments@yourschool.com");
