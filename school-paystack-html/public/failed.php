c64ed3216d39ba697c6f8217392523be1e16456a
Big Quams <bigquamsmedia024@gmail.com>
2026-01-17 10:55:17 +0100

    Delete school-paystack-html/public directoryi

--- school-paystack-html/public/failed.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body style="text-align:center;padding:50px;">
-<h1>❌ Payment Failed</h1>
-<p>Something went wrong. Please try again.</p>
-<a href="index.php">Retry Payment</a>
-</body>
-</html>
