482830b0d92986ccf021d9989166c5501d288afe
Big Quams <bigquamsmedia024@gmail.com>
2026-01-17 11:08:00 +0100

    Delete app/Services directory

--- app/Services/EmailService.php
+++
@@ -1,7 +0,0 @@
-<?php
-class EmailService {
-    public static function send($to, $subject, $message) {
-        $headers = "From: " . getenv("SMTP_USER");
-        mail($to, $subject, $message, $headers);
-    }
-}
--- app/Services/WhatsAppService.php
+++
@@ -1,6 +0,0 @@
-<?php
-class WhatsAppService {
-    public static function link($phone, $message) {
-        return "https://wa.me/$phone?text=" . urlencode($message);
-    }
-}
