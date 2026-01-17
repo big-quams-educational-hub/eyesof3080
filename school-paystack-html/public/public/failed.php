01c1ef6ef0e814cce50fc1e34206d7a3c19d7c61
Big Quams <bigquamsmedia024@gmail.com>
2026-01-17 10:48:03 +0100

    Create failed.php

---
+++ school-paystack-html/public/public/failed.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body style="text-align:center;padding:50px;">
+<h1>❌ Payment Failed</h1>
+<p>Something went wrong. Please try again.</p>
+<a href="index.php">Retry Payment</a>
+</body>
+</html>
