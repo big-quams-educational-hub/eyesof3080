d4a124b6b4014f8722f9e966cdf294ee2b02bb6a
Big Quams <bigquamsmedia024@gmail.com>
2026-06-16 11:55:51 +0100

    Delete config.php

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("PAYSTACK_SECRET", "sk_live_7bdb0f70acc96ba9c4e15936c7ea4a8eac14ef93");
-define("ADMIN_PHONE", "2349049871643"); // WhatsApp number
-define("SCHOOL_EMAIL", "bigquamsmedia024@gmail.com");
